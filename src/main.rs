fb22009c08a27b5c7c05bb8e795a6fc070db8f94
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2024-01-25 19:36:22 -0500

    Add functionality to obtain vector of files in pwd

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-use std::{env, path};
+use std::{env, path, fs};
 use std::path::Path;
 use std::{fs::File, io::Read};
 use sha2::{Sha256, Digest};
 use anyhow::Result;
-// use walkdir::WalkDir;
+use walkdir::WalkDir;
 use clap::{arg, command, value_parser, ArgAction, Command};
+use std::fs::metadata;
 
 /// Computes the sha256 of a file
 fn compute_sha_256(file: &str) -> Result<String> {
@@ -28,8 +29,22 @@ fn compute_sha_256(file: &str) -> Result<String> {
     Ok(format!("{:x}", hasher.finalize()))
 }
 
-fn get_files_in_path(path: &str) {
-    return ();
+
+/// Returns a Vec<path::PathBuf> of files in pwd
+fn get_files_in_pwd() -> Vec<path::PathBuf> {
+    let mut files: Vec<path::PathBuf> = Vec::new();
+
+    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+        let entry_path = entry.into_path();
+
+        if entry_path.is_dir() {
+            continue;
+        }
+
+        files.push(entry_path);
+    }
+
+    return files;
 }
 
 
@@ -37,9 +52,10 @@ fn get_files_in_path(path: &str) {
 /// Useful for testing purposes
 fn pwd() -> std::io::Result<()> {
     let current_directory = env::current_dir()?;
-    println!("\n{}", current_directory.display());
+    println!("{}", current_directory.display());
     Ok(())
 }
+
 
 /// Changes from current directory to `root`
 /// Analogous to the `cd` command found in Linux and MacOS
@@ -59,7 +75,6 @@ fn main() {
         .arg(arg!(--regex <STRING>).required(false))
         .get_matches();
 
-    let _ = pwd();
 
     let directory: String = matches.get_one::<String>("directory")
         .expect("required")
@@ -67,5 +82,5 @@ fn main() {
 
     let _ = cd(&directory);
 
-    let _ = pwd();
+    let files = get_files_in_pwd();
 }
