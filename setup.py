16d17b631343b4365c9c3beb87ec17658cc49355
Brandon J. T. Noguera <software_dev.snore288@slmail.me>
2024-05-18 15:21:07 -0400

    Add setup.py and the required files to prepare for upload to PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name            = "Doopie",
+    version         = "0.1.0",
+    description     = "Doopie is a command line tool that finds duplicate files in a given directory",
+    url             = "https://github.com/bjtn1/doopie",
+    author          = "Brandon Jose Tenorio Noguera",
+    author_email    = "software_dev.punctuate119@slmail.me",
+    license         = "MIT",
+    packages        = ["src"],
+    keywords        = "files duplicates",
+)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 # !/usr/bin/env python3
 
+# TODO I need a way to skip common files like `__init__.py` and others...
+
 import argparse
 import hashlib
 import os
@@ -11,10 +13,9 @@ from alive_progress import alive_bar
 from tabulate import tabulate
 
 # source https://gist.githubusercontent.com/rene-d/9e584a7dd2935d0f461904b9f2950007/raw/e2e58ccf955475d8066338a4e538c52debc06a06/colors.py
-""" ANSI color codes """
+# ANSI color codes
 BOLD            = "\033[1m"
 END             = "\033[0m"
-
 
 def file_has_read_permissions(file_mode):
     return stat.S_IRUSR & file_mode
@@ -39,16 +40,16 @@ def find_dupes(path, ignore_list=None, ignore_file=None):
                 ignore_list = []
                 # TODO add alive_progress to this
             with open(ignore_file, 'r') as file:
-                for line in file:
-                    ignore_list.extend(line.strip().split())
+                with alive_bar(title=f"{BOLD}Skipping files\t\t\t\t{END}") as bar:
+                    for line in file:
+                        ignore_list.extend(line.strip().split())
+                        bar()
 
-        """
-        This walks every directory inside of `path` and looks at every file within every directory
-        We first get the full path of every file
-        Then we get the size in bytes of every file and add the file path to size_dict[file_size]
-
-        We do it this way because it is possible for two completely different files to have the exact same size
-        """
+        # This walks every directory inside of `path` and looks at every file within every directory
+        # We first get the full path of every file
+        # Then we get the size in bytes of every file and add the file path to size_dict[file_size]
+        #
+        # We do it this way because it is possible for two completely different files to have the exact same size
         with alive_bar(title=f"{BOLD}Scanning files\t\t\t\t{END}") as bar:
             for current_path, _, files_in_current_path in os.walk(path):
                 total_files_in_path += len(files_in_current_path)
@@ -86,12 +87,10 @@ def find_dupes(path, ignore_list=None, ignore_file=None):
                     size_dict[file_size].append(full_path_to_file)
         
 
-        """
-        Now we iterate over the values of size_dict (which are lists whose size is either 1 or greater than 1)
-        If the list size is equal to one, we can be certain that this is the only file of its kind in this directory (in other words, this file is unique), so we add it
-        to unique_files list
-        If the list size is greater than one, we compute the file has for every file within the list and add it to hash_dict[file_hash]
-        """
+        # Now we iterate over the values of size_dict (which are lists whose size is either 1 or greater than 1)
+        # If the list size is equal to one, we can be certain that this is the only file of its kind in this directory (in other words, this file is unique), so we add it
+        # to unique_files list
+        # If the list size is greater than one, we compute the file has for every file within the list and add it to hash_dict[file_hash]
         with alive_bar(title=f"{BOLD}Hashing files\t\t\t\t{END}") as bar:
             for file_list in size_dict.values():
                 if len(file_list) == 1:
@@ -114,12 +113,10 @@ def find_dupes(path, ignore_list=None, ignore_file=None):
                             continue
 
 
-        """
-        Now we do the exact same thing we did in the previous step
-        We iterate over hash_dict.values()
-        If the size of the list == 1, we add it to unique_files
-        Otherwise, we can be certain that the files in the list are duplicates, so we can flatten the list and add every file to duplicate_files
-        """
+        # Now we do the exact same thing we did in the previous step
+        # We iterate over hash_dict.values()
+        # If the size of the list == 1, we add it to unique_files
+        # Otherwise, we can be certain that the files in the list are duplicates, so we can flatten the list and add every file to duplicate_files
         with alive_bar(title=f"{BOLD}Finding duplicates{END}\t\t\t") as bar:
             for file_list in hash_dict.values():
                 if len(file_list) == 1:
@@ -143,9 +140,7 @@ def find_dupes(path, ignore_list=None, ignore_file=None):
                     continue
                 duplicate_files_size += os.path.getsize(file)
 
-        """
-        Write duplicate_files to a file called `duplicate_files.txt` found in `path`
-        """
+        # Write duplicate_files to a file called `duplicate_files.txt` found in `path`
         # Create and clear the file before appending to it
         duplicate_files_txt = os.path.join(path, "duplicate_files.txt")
         with open(duplicate_files_txt, "w") as output_file:
@@ -161,9 +156,7 @@ def find_dupes(path, ignore_list=None, ignore_file=None):
 
         print(f"\n{BOLD}{os.path.abspath(duplicate_files_txt)}{END} was successfully written")
 
-        """
-        Print out info about what we just did
-        """
+        # Print out info about what we just did
         def format_num(size):
             return f"{BOLD}{size:_}{END}"
 
@@ -176,7 +169,7 @@ def find_dupes(path, ignore_list=None, ignore_file=None):
             [f"Total size of files",             f"{format_num(total_path_size)} bytes"],
             [f"Total size of unique files",      f"{format_num(unique_files_size)} bytes"],
             [f"Total size of duplicate files",   f"{format_num(duplicate_files_size)} bytes"],
-            [f"Total time elapsed", f"{BOLD}{elapsed_time:.2f}{END} seconds"],
+            [f"Total time elapsed",              f"{BOLD}{elapsed_time:.2f}{END} seconds"],
         ]
 
         print(tabulate(table, tablefmt="fancy_grid"))
